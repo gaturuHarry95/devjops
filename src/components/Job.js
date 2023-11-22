5dba94cedf843e211b99a99a0abb594d59126bad
gaturuHarry95 <gaturuharrison@gmail.com>
2023-11-22 11:30:42 +0300

    modified files

---
+++ src/components/Job.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import data from './data'
+
+const Job = () => {
+  
+  return (
+    <div>
+      {
+ 
+      }
+    </div>
+  )
+}
+
+export default Job
---
+++ src/components/JobList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const JobList = (record) => {
+  return (
+    <div>
+      <h1>{record.company}</h1>
+    </div>
+  )
+}
+
+export default JobList
